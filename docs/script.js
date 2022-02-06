c98b7b9be20041bedb618bd6561ffcbc6f806b1a
erma <agpritts@gmail.com>
2022-02-05 20:51:38 -0500

    load tasks capability added

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,15 @@
 var timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 var tasks = $("textarea");
 var saveBtn = $(".saveBtn")
+
 var displayTime = function() {
-    var time = moment().format('LL');
+    var time = moment().format('dddd, MMMM Do');
     $("#currentDay").html(time)
-}
+};
 
 $(document).ready(function() {
     displayTime();
+    loadTask();
 });
 
 $("h4").each(function(index) {
@@ -24,14 +26,22 @@ $("textarea").each(function(index) {
     else {
         $(this).addClass("future")
     }
-    /* var loadTask = localStorage.getItem($(this).data("hour"));
-    console.log(loadTask);
-    $(this).text(loadTask); */
 });
+
+var loadTask = function() {
+    var storage = window.localStorage
+    storage.getItem($)
+    for (var i = 0; i < storage.length; i++) {
+        storage.key(i);
+        var cKey = storage.key(i);
+        var texty = cKey + "M";
+        $("#" + texty).append(storage.getItem(cKey));
+    }
+};
 
 var saveTasks = function(event) {
     event.preventDefault();
     localStorage.setItem($(this).siblings("h4").attr("id"), $(this).siblings("textarea").val());
 };
 
-saveBtn.on("click", saveTasks)
+saveBtn.on("click", saveTasks);
